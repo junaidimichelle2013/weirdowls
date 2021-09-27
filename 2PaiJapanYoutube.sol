c2e77788acd46875ac16045510be977ac5f7ee02
Michelle122103 <89189604+junaidimichelle2013@users.noreply.github.com>
2021-09-27 15:45:38 +0700

    Create 2PaiJapanYoutube.sol

---
+++ 2PaiJapanYoutube.sol
@@ -0,0 +1,40 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract PaiJapanYoutube is ERC721, ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("PaiJapanYoutube", "YJ2PAI") public {}
+    
+    function mint2Pai() public returns (uint256) {
+
+        _tokenIds.increment(); 
+        uint256 newItemId = _tokenIds.current();
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, "glitter.json");
+
+        return newItemId;
+    }
+    
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://raw.githubusercontent.com/2pai/nft-erc721/main/item/";
+    }
+    
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+    
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+}
